2e0f45af716afcf0e22acaafeedc6a13fc92666f
ziad abo alkamar <ziad-lovelyboy@hotmail.com>
2022-10-27 18:14:25 +0300

    ziad

--- app-assets/js/scripts/pages/users-contacts.js
+++ app-assets/js/scripts/pages/users-contacts.js
@@ -14,13 +14,63 @@ $(document).ready(function() {
         searching: false,
         info: false,
         "bLengthChange" : false, //thought this line could hide the LengthMenu
-        "bInfo":false,        
+        "bInfo":false,   
+        dom: 'Bfrtip',
+        buttons: [
+            {
+                extend: 'collection',
+                className: 'btn btn-outline-secondary dropdown-toggle mr-2 mt-50',
+                text:  'Export Report',
+                buttons: [
+                    {
+                        extend: 'print',
+                        text: 'Print',
+                        className: 'dropdown-item',
+                        exportOptions: {columns: [0, 1, 2, 3, 4]}
+                    },
+                    {
+                        extend: 'csv',
+                        text:  'Csv',
+                        className: 'dropdown-item',
+                        exportOptions: {columns: [0, 1, 2, 3, 4]}
+                    },
+                    {
+                        extend: 'excel',
+                        text:  'Excel',
+                        className: 'dropdown-item',
+                        exportOptions: {columns: [0, 1, 2, 3, 4]}
+                    },
+                    {
+                        extend: 'pdf',
+                        text: 'Pdf',
+                        className: 'dropdown-item',
+                        exportOptions: {columns: [0, 1, 2, 3, 4]}
+                    },
+                    {
+                        extend: 'copy',
+                        text:  'Copy',
+                        className: 'dropdown-item',
+                        exportOptions: {columns: [0, 1, 2, 3, 4]}
+                    }
+                ],
+                init: function (api, node, config) {
+                    $(node).removeClass('btn-secondary');
+                    $(node).parent().removeClass('btn-group');
+                    setTimeout(function () {
+                        $(node).closest('.dt-buttons').removeClass('btn-group').addClass('d-inline-flex');
+                    }, 50);
+                }
+            },
+    
+    
+        ],     
         language: {
             'paginate': {
               'previous': '<i class="la la-angle-left"></i>',
               'next': '<i class="la la-angle-right"></i>'
             }
-          }
+          } ,
+          
     });
     // Set the search textbox functionality in sidebar
     $('#search-contacts').on( 'keyup', function () {
@@ -40,5 +90,8 @@ $(document).ready(function() {
             radioClass: 'iradio_square-blue',
         });
     });
+
+    $('.buttons-copy, .buttons-csv, .buttons-print, .buttons-pdf, .buttons-excel').addClass('btn btn-primary mr-1');
+
 
 });
