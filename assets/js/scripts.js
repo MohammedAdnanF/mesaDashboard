ac3bcb893bbcab2aa7be87457fc2398bfc36f7e4
Muhammad Alfarra <mohmed.adnanf@gmail.com>
2022-11-28 08:16:12 +0200

    Last update on admin panel

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -12,27 +12,64 @@
 
   // $(".pickatime").pickatime();
 
-  $(".number-tab-steps").steps({
-    headerTag: "h6",
-    bodyTag: "fieldset",
-    transitionEffect: "fade",
-    titleTemplate: '#index#',
-    labels: {
-      finish: 'Submit'
-    },
-    onFinished: function (event, currentIndex) {
-      alert("Form submitted.");
-    }
-  });
+  if ($('.number-tab-steps').length) {
+    $(".number-tab-steps").steps({
+      headerTag: "h6",
+      bodyTag: "fieldset",
+      transitionEffect: "fade",
+      titleTemplate: '#index#',
+      labels: {
+        finish: 'Submit'
+      },
+      onFinished: function (event, currentIndex) {
+        alert("Form submitted.");
+      }
+    });
+  }
 
-  $(".select2").select2();
-  $(".select2.no-search").select2({
-    minimumResultsForSearch: -1
-  });
+  if ($('.select2').length) {
+    $(".select2").select2();
+    $(".select2.no-search").select2({
+      minimumResultsForSearch: -1
+    });
+  }
 
-  $('.icheck').iCheck({
-    checkboxClass: 'icheckbox',
-  });
+  if ($('.icheck').length) {
+    $('.icheck').iCheck({
+      checkboxClass: 'icheckbox',
+    });
+  }
+
+  var currentMonth = moment().format('YYYY-MM');
+	var nextMonth    = moment().add('month', 1).format('YYYY-MM');
+
+  var events = [{
+		date: currentMonth + '-' + '10',
+		title: 'Persian Kitten Auction',
+		location: 'Center for Beautiful Cats'
+	}, {
+		date: currentMonth + '-' + '19',
+		title: 'Cat Frisbee',
+		location: 'Jefferson Park'
+	}, {
+		date: currentMonth + '-' + '23',
+		title: 'Kitten Demonstration',
+		location: 'Center for Beautiful Cats'
+	}, {
+		date: nextMonth + '-' + '07',
+		title: 'Small Cat Photo Session',
+		location: 'Center for Cat Photography'
+	}];
+
+  /********************************************
+  *				Selected Date				*
+  ********************************************/
+	$('#clndr-selected-date').clndr({
+		template: $('#clndr-template').html(),
+		events: events,
+		trackSelectedDate: true,
+	});
+
 
   /*
   NOTE:
