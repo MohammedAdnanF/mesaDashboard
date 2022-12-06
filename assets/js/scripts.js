efe5b0ff0ca0224c2523e8a460ec6d63c5599873
Muhammad Alfarra <mohmed.adnanf@gmail.com>
2022-12-06 14:19:54 +0200

    dropzones configurations

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -19,7 +19,7 @@
       transitionEffect: "fade",
       titleTemplate: '#index#',
       labels: {
-        finish: 'Submit'
+        finish: 'Publish'
       },
       onFinished: function (event, currentIndex) {
         alert("Form submitted.");
@@ -40,36 +40,47 @@
     });
   }
 
-  var currentMonth = moment().format('YYYY-MM');
-	var nextMonth    = moment().add('month', 1).format('YYYY-MM');
+  if($('#clndr-selected-date').length){
+    var currentMonth = moment().format('YYYY-MM');
+    var nextMonth    = moment().add('month', 1).format('YYYY-MM');
+  
+    var events = [{
+      date: currentMonth + '-' + '10',
+      title: 'Persian Kitten Auction',
+      location: 'Center for Beautiful Cats'
+    }, {
+      date: currentMonth + '-' + '19',
+      title: 'Cat Frisbee',
+      location: 'Jefferson Park'
+    }, {
+      date: currentMonth + '-' + '23',
+      title: 'Kitten Demonstration',
+      location: 'Center for Beautiful Cats'
+    }, {
+      date: nextMonth + '-' + '07',
+      title: 'Small Cat Photo Session',
+      location: 'Center for Cat Photography'
+    }];
 
-  var events = [{
-		date: currentMonth + '-' + '10',
-		title: 'Persian Kitten Auction',
-		location: 'Center for Beautiful Cats'
-	}, {
-		date: currentMonth + '-' + '19',
-		title: 'Cat Frisbee',
-		location: 'Jefferson Park'
-	}, {
-		date: currentMonth + '-' + '23',
-		title: 'Kitten Demonstration',
-		location: 'Center for Beautiful Cats'
-	}, {
-		date: nextMonth + '-' + '07',
-		title: 'Small Cat Photo Session',
-		location: 'Center for Cat Photography'
-	}];
+    $('#clndr-selected-date').clndr({
+      template: $('#clndr-template').html(),
+      events: events,
+      trackSelectedDate: true,
+    });
+  }
 
-  /********************************************
-  *				Selected Date				*
-  ********************************************/
-	$('#clndr-selected-date').clndr({
-		template: $('#clndr-template').html(),
-		events: events,
-		trackSelectedDate: true,
-	});
+  if($('.repeater').length){
+    $('.repeater').repeater();
+  }
 
+  // File Upload (--Dropzones--)
+  Dropzone.autoDiscover = false;
+  $('.dropzone.single-file').each(function(){
+    $(this).dropzone({ 
+      maxFiles: 1,
+      url: "#"
+    });
+  });
 
   /*
   NOTE:
